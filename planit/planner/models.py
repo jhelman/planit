dd1dfe681bfae65a52efa0232ad8823d3aad2d7d
Dan Vinegrad <dan.vinegrad@gmail.com>
2012-04-23 21:52:37 -0700

    load basic schedule from models

--- planit/planner/models.py
+++ planit/planner/models.py
@@ -4,3 +4,7 @@ class Course(models.Model):
     identifier = models.CharField(max_length=20) # i.e. CS 106A
     name = models.CharField(max_length=50) #i.e. Programming Methodology
     description = models.CharField(max_length=300)
+    units = models.IntegerField()
+    
+    def __unicode__(self):
+        return self.identifier
--- planit/planner/views.py
+++ planit/planner/views.py
@@ -3,4 +3,27 @@ from django.template import RequestContext
 from models import Course
 
 def index(request):
-    return render_to_response('planner/index.html', context_instance=RequestContext(request))
+    args = {}
+    terms = [{}, {}, {}]
+    terms[0]['courses'] = []
+    terms[0]['name'] = 'Autumn 2011-2012'
+    terms[0]['condensedName'] = terms[0]['name'].replace(' ','')
+    terms[1]['courses'] = []
+    terms[1]['name'] = 'Winter 2011-2012'
+    terms[1]['condensedName'] = terms[1]['name'].replace(' ','')
+    terms[2]['courses'] = []
+    terms[2]['name'] = 'Spring 2011-2012'
+    terms[2]['condensedName'] = terms[2]['name'].replace(' ','')
+    terms[0]['courses'].append(Course.objects.filter(identifier='CS 106A')[0])
+    terms[0]['courses'].append(Course.objects.filter(identifier='Chem 31A')[0])
+    terms[0]['courses'].append(Course.objects.filter(identifier='Econ 1A')[0])
+    terms[1]['courses'].append(Course.objects.filter(identifier='PWR 1')[0])
+    terms[1]['courses'].append(Course.objects.filter(identifier='Econ 1B')[0])
+    terms[2]['courses'].append(Course.objects.filter(identifier='CS 106B')[0])
+    terms[2]['courses'].append(Course.objects.filter(identifier='CS 103')[0])
+    terms[2]['courses'].append(Course.objects.filter(identifier='Physics 41')[0])
+    for term in terms:
+        for course in term['courses']:
+            setattr(course, 'condensedID', course.identifier.replace(' ', ''))
+    args['terms'] = terms
+    return render_to_response('planner/index.html', args, context_instance=RequestContext(request))
