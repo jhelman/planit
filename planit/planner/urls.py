a4a3f5dc007b84f23d2832ca7bf86dfccbe78006
Dan Vinegrad <dan.vinegrad@gmail.com>
2012-04-23 20:18:12 -0700

    load the template from views.py rather than directly from urls

--- planit/planner/urls.py
+++ planit/planner/urls.py
@@ -2,5 +2,5 @@ from django.conf.urls import patterns, include, url
 from django.views.generic.simple import direct_to_template
 
 urlpatterns = patterns('planner.views',
-    url(r'^$', direct_to_template, {'template': 'planit/index.html'}),
+    url(r'^$', 'index'),
 )
--- planit/planner/views.py
+++ planit/planner/views.py
@@ -1,1 +1,6 @@
-# Create your views here.
+from django.shortcuts import render_to_response
+from django.template import RequestContext
+from models import Course
+
+def index(request):
+    return render_to_response('planner/index.html', context_instance=RequestContext(request))
