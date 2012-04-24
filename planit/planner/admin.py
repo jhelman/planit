f6cbcb2eed4d3496e9258a5ada30802e3b241d66
Dan Vinegrad <dan.vinegrad@gmail.com>
2012-04-23 19:59:12 -0700

    added very basic course model, added some courses to db

---
+++ planit/planner/admin.py
@@ -0,0 +1,4 @@
+from planner.models import Course
+from django.contrib import admin
+
+admin.site.register(Course)
--- planit/planner/models.py
+++ planit/planner/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+class Course(models.Model):
+    identifier = models.CharField(max_length=20) # i.e. CS 106A
+    name = models.CharField(max_length=50) #i.e. Programming Methodology
+    description = models.CharField(max_length=300)
