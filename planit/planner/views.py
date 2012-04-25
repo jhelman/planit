41278d92b55bc6ac6efc89c6ec6c7836dcce9858
Dan Vinegrad <dan.vinegrad@gmail.com>
2012-04-24 19:18:14 -0700

    show 4 years with template-based code

--- planit/planner/views.py
+++ planit/planner/views.py
@@ -4,6 +4,7 @@ from models import Course
 
 def index(request):
     args = {}
+    years = [{}, {}, {}, {}]
     terms = [{}, {}, {}]
     terms[0]['courses'] = []
     terms[0]['name'] = 'Autumn 2011-2012'
@@ -22,8 +23,22 @@ def index(request):
     terms[2]['courses'].append(Course.objects.filter(identifier='CS 106B')[0])
     terms[2]['courses'].append(Course.objects.filter(identifier='CS 103')[0])
     terms[2]['courses'].append(Course.objects.filter(identifier='Physics 41')[0])
+
     for term in terms:
         for course in term['courses']:
             setattr(course, 'condensedID', course.identifier.replace(' ', ''))
+    years[0]['year'] = '2011-2012'
+    years[0]['name'] = 'Freshman'
+    years[0]['terms'] = terms
+    years[1]['year'] = '2012-2013'
+    years[1]['name'] = 'Sophomore'
+    years[1]['terms'] = terms
+    years[2]['year'] = '2013-2014'
+    years[2]['name'] = 'Junior'
+    years[2]['terms'] = terms
+    years[3]['year'] = '2014-2015'
+    years[3]['name'] = 'Senior'
+    years[3]['terms'] = terms
     args['terms'] = terms
+    args['years'] = years
     return render_to_response('planner/index.html', args, context_instance=RequestContext(request))
