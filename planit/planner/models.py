8031b3719aa2d418fcfc93764904f774f29c3b1e
Dan Vinegrad <dan.vinegrad@gmail.com>
2012-04-29 22:46:49 -0700

    added south app, migrated db to include start/end times

--- planit/planner/models.py
+++ planit/planner/models.py
@@ -1,10 +1,13 @@
 from django.db import models
+import datetime
 
 class Course(models.Model):
     identifier = models.CharField(max_length=20) # i.e. CS 106A
     name = models.CharField(max_length=50) #i.e. Programming Methodology
     description = models.CharField(max_length=300)
     units = models.IntegerField()
+    startTime = models.TimeField(default=datetime.time(9,0))
+    endTime = models.TimeField(default=datetime.time(9,50))
     
     def __unicode__(self):
         return self.identifier
