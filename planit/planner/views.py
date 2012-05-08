7512034f2f1b16c24cfecfd15a55f6830bdb8546
Dan Vinegrad <dan.vinegrad@gmail.com>
2012-05-08 14:26:28 -0700

    removed commented out code in views.py, modified offering times in db

--- planit/planner/views.py
+++ planit/planner/views.py
@@ -63,54 +63,6 @@ def index(request):
             totalUnits += units
         years[year_num]['terms'] = terms
             
-    
-    
-    
-    
-    # for i in range(4):
-    #     terms = [{}, {}, {}]
-    #     terms[0]['courses'] = []
-    #     terms[0]['name'] = 'Autumn ' + years[i]['year']
-    #     terms[0]['condensedName'] = terms[0]['name'].replace(' ','')
-    #     terms[1]['courses'] = []
-    #     terms[1]['name'] = 'Winter ' + years[i]['year']
-    #     terms[1]['condensedName'] = terms[1]['name'].replace(' ','')
-    #     terms[2]['courses'] = []
-    #     terms[2]['name'] = 'Spring ' + years[i]['year']
-    #     terms[2]['condensedName'] = terms[2]['name'].replace(' ','')
-    #     terms[0]['courses'].append(Course.objects.filter(identifier='CS 106A')[0])
-    #     terms[0]['courses'].append(Course.objects.filter(identifier='Chem 31A')[0])
-    #     terms[0]['courses'].append(Course.objects.filter(identifier='Econ 1A')[0])
-    #     terms[1]['courses'].append(Course.objects.filter(identifier='PWR 1')[0])
-    #     terms[1]['courses'].append(Course.objects.filter(identifier='Econ 1B')[0])
-    #     terms[2]['courses'].append(Course.objects.filter(identifier='CS 106B')[0])
-    #     terms[2]['courses'].append(Course.objects.filter(identifier='CS 103')[0])
-    #     terms[2]['courses'].append(Course.objects.filter(identifier='Physics 41')[0])
-    # 
-    #     for term in terms:
-    #         units = 0
-    #         for course in term['courses']:
-    #             setattr(course, 'condensedID', course.identifier.replace(' ', ''))
-    #             units += course.units
-    #             days = []
-    #             start = 23
-    #             for weekday in course.weekdays:
-    #                 if weekday == 'M':
-    #                     days.append(start)
-    #                 if weekday == 'T':
-    #                     days.append(start + 1)
-    #                 if weekday == 'W':
-    #                     days.append(start + 2)
-    #                 if weekday == 'R':
-    #                     days.append(start + 3)
-    #                 if weekday == 'F':
-    #                     days.append(start + 4)
-    #             setattr(course, 'days', days)
-    #                     
-    #                     
-    #         term['units'] = units
-    #         totalUnits += units
-    #     years[i]['terms'] = terms
             
     args['years'] = years
     args['totalUnits'] = totalUnits
