b6e15e5b4ee9868b6c574e0cd2abe1edc1abb9f1
ls <ls@lsl.local>
2012-06-04 20:52:50 -0700

    urls for userS

--- planit/planit/urls.py
+++ planit/planit/urls.py
@@ -14,6 +14,7 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^accounts/login/$', 'django.contrib.auth.views.login'),
     url(r'^accounts/', include('registration.backends.default.urls')),
     url(r'', include('planner.urls')),
 )
