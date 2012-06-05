bcffec60e7bb24036df491123299902ecdccd7b6
ls <ls@lsl.local>
2012-06-04 22:54:49 -0700

    nothing, to see here

--- planit/filldb.py
+++ planit/filldb.py
@@ -172,7 +172,7 @@ def filldb():
     for i in range(3):
         t=Term(i)
         t.save()
-    fnames = ['all2.xml']#cs.xml', 'math.xml', 'ihum.xml', 'physics.xml', 'humbio.xml', 'econ.xml', 'me.xml', 'engr.xml']
+    fnames = ['cs.xml', 'math.xml', 'ihum.xml', 'physics.xml', 'humbio.xml', 'econ.xml', 'me.xml', 'engr.xml']
     for fname in fnames:
         parse_document(fname)
     u=University(name='Stanford',max_units_per_quarter=20)
