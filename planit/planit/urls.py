4738664ce487478551710cd89ca6c387d5c9575f
ls <ls@lsl.local>
2012-06-09 19:50:23 -0700

    deleted planner.

--- planit/planit/urls.py
+++ planit/planit/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import patterns, include, url
-from planner.name_reg import NameRegistrationForm
+from name_reg import NameRegistrationForm
 
 from django.views.generic.simple import direct_to_template
 from registration.views import activate
